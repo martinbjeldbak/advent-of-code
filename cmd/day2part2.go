d70318565ec8318129ba35205666a0e7c4e4bb13
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2021-12-07 23:34:03 +1100

    Add day2part2 solution

--- cmd/day2part2.go
+++ cmd/day2part2.go
@@ -23,10 +23,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func day2(commands []string) (int, error) {
+func day2part2(commands []string) (int, error) {
 
 	hPos := 0
 	depth := 0
+	aim := 0
 
 	for _, commandSet := range commands {
 		instructions := strings.Split(commandSet, " ")
@@ -40,23 +41,23 @@ func day2(commands []string) (int, error) {
 
 		if command == "forward" {
 			hPos += units
+			depth += aim * units
 		}
 		if command == "down" {
-			depth += units
+			aim += units
 		}
 		if command == "up" {
-			depth -= units
+			aim -= units
 		}
-
 	}
 	return hPos * depth, nil
 }
 
-// day2Cmd represents the day2 command
-var day2Cmd = &cobra.Command{
-	Use: "day2",
+// day2part2Cmd represents the day2part2 command
+var day2part2Cmd = &cobra.Command{
+	Use: "day2part2",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		res, err := day2(inputData)
+		res, err := day2part2(inputData)
 
 		if err != nil {
 			return err
@@ -69,15 +70,15 @@ var day2Cmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(day2Cmd)
+	rootCmd.AddCommand(day2part2Cmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// day2Cmd.PersistentFlags().String("foo", "", "A help for foo")
+	// day2part2Cmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// day2Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// day2part2Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
